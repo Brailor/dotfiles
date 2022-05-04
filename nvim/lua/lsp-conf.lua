2f068d872059cd3e0677157560552357b254c304
Viktor Ohád <viktor.ohad@protonmail.com>
2022-05-04 19:36:50 +0200

    add and configure lua lsp

--- nvim/lua/lsp-conf.lua
+++ nvim/lua/lsp-conf.lua
@@ -1,3 +1,38 @@
+
+local sumneko_root_path = "/Users/viktor.ohad/repos/github.com/other/lua-language-server"
+local sumneko_binary = sumneko_root_path .. "/bin/lua-language-server"
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+
+require'lspconfig'.sumneko_lua.setup {
+  cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" },
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Setup your lua path
+        path = vim.split(package.path, ';'),
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap=true, silent=true }
@@ -31,12 +66,12 @@ end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'pyright', 'tsserver', 'bashls', 'gopls' }
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local servers = { 'pyright', 'tsserver', 'bashls', 'gopls', 'rust_analyzer' }
 for _, lsp in pairs(servers) do
   require('lspconfig')[lsp].setup {
     on_attach = on_attach,
-    capabilites = capabilites
+    capabilities = capabilities
+
   }
 end
 
